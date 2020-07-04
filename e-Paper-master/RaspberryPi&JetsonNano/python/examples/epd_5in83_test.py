60dad1393f62abbeb0dd4b3dda4991460d8d7a25
Florian Goelz <florian.goelz@outlook.de>
2020-07-04 19:56:33 +0200

    test

---
+++ e-Paper-master/RaspberryPi&JetsonNano/python/examples/epd_5in83_test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd5in83
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd5in83 Demo")
+    
+    epd = epd5in83.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear()
+    
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+    
+    # Drawing on the Horizontal image
+    logging.info("1.Drawing on the Horizontal image...")
+    Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(Himage)
+    
+    logging.info("3.read bmp file")
+    Himage = Image.open(os.path.join(picdir, 'example.bmp'))
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(2)
+    
+    
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd5in83.epdconfig.module_exit()
+    exit()
--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ if os.path.exists(libdir):
     sys.path.append(libdir)
 
 import logging
-from waveshare_epd import epd5in83
+from 'waveshare_epd' import epd5in83
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
@@ -22,43 +22,16 @@ try:
     epd.init()
     epd.Clear()
     
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    
     # Drawing on the Vertical image
     logging.info("2.Drawing on the Vertical image...")
     Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Limage)
-    draw.text((2, 0), 'hello world', font = font18, fill = 0)
-    draw.text((2, 20), '5.83inch epd', font = font18, fill = 0)
-    draw.text((20, 50), u'微雪电子', font = font18, fill = 0)
-    draw.line((10, 90, 60, 140), fill = 0)
-    draw.line((60, 90, 10, 140), fill = 0)
-    draw.rectangle((10, 90, 60, 140), outline = 0)
-    draw.line((95, 90, 95, 140), fill = 0)
-    draw.line((70, 115, 120, 115), fill = 0)
-    draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    draw.rectangle((10, 150, 60, 200), fill = 0)
-    draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
-    epd.display(epd.getbuffer(Limage))
-    time.sleep(2)
     
     logging.info("3.read bmp file")
     Himage = Image.open(os.path.join(picdir, '5in83.bmp'))
     epd.display(epd.getbuffer(Himage))
     time.sleep(2)
-    
-    logging.info("4.read bmp file on window")
-    Himage2 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-    Himage2.paste(bmp, (50,10))
-    epd.display(epd.getbuffer(Himage2))
-    time.sleep(2)
 
-    logging.info("Clear...")
-    epd.init()
-    epd.Clear()
-    
     logging.info("Goto Sleep...")
     epd.sleep()
     
