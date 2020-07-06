e2d2cca6fda02233efc8966bfbd4379bcb67de6c
Florian Goelz <florian.goelz@outlook.de>
2020-07-06 18:41:04 +0200

    test

--- src/display.py
+++ src/display.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+
+from WeatherData import WeatherData
+from CurrentWeather import CurrentWeather
+from datetime import date
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+iconsdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'icons')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
@@ -25,16 +29,52 @@ try:
     
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    
-    # Drawing on the Horizontal image
-    logging.info("1.Drawing on the Horizontal image...")
-    Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(Himage)
-    
-    logging.info("3.read bmp file")
-    Himage = Image.open(os.path.join(picdir, 'example.bmp'))
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
+
+    icons = {
+        "01d": "clear",
+        "01n": "clear",
+        "02d": "few_clouds",
+        "02n": "few_clouds",
+        "03d": "scattered_clouds",
+        "03n": "scattered_clouds",
+        "04d": "broken_clouds",
+        "04n": "broken_clouds",
+        "09d": "shower_rain",
+        "09n": "shower_rain",
+        "10d": "rain",
+        "10n": "rain",
+        "11d": "thunderstorm",
+        "11n": "thunderstorm",
+        "13d": "snow",
+        "13n": "snow",
+        "50d": "mist",
+        "50n": "mist"
+    }
+
+    while(1):
+        
+        # Drawing on the Horizontal image
+        logging.info("1.Drawing on the Horizontal image...")
+        Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+        draw = ImageDraw.Draw(Limage)
+        
+        #get current weather data
+        weatherdata = WeatherData('49.591', '8.646', 'bcdf63a0553e42805e8f1111bcbd94e5')
+        currentWeather = weatherdata.getData()
+
+        #date
+        today = date.today()
+        draw.text((5, 8), str(today.day) + "." + str(today.month) + "." + str(today.year), font = font18, fill = 0)
+
+        #icon
+        bmpIcon = Image.open(os.path.join())
+
+
+        logging.info("3.read bmp file")
+        iconbmp = Image.open(os.path.join(iconsdir, icons[currentWeather.currentIcon] + '.bmp'))
+        Himage.paste(iconbmp, (5, 30))
+        epd.display(epd.getbuffer(Himage))
+        time.sleep(1800)
     
     
 except IOError as e:
